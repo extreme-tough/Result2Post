4f0b85505061dc7e0415989b8c6ed152f1407fb7
Cuebiz <support@cuebiztech.com>
2022-09-06 11:44:48 +0530

    Initial Commit

---
+++ Result2Post/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.chromiumWebBrowser1 = new CefSharp.WinForms.ChromiumWebBrowser();
+            this.keywords = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPostCount = new System.Windows.Forms.TextBox();
+            this.txtHeader = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.keyword = new System.Windows.Forms.LinkLabel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.statusStrip.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(647, 531);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(111, 33);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(764, 531);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(111, 33);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "&Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // chromiumWebBrowser1
+            // 
+            this.chromiumWebBrowser1.ActivateBrowserOnCreation = false;
+            this.chromiumWebBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chromiumWebBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.chromiumWebBrowser1.Name = "chromiumWebBrowser1";
+            this.chromiumWebBrowser1.Size = new System.Drawing.Size(863, 356);
+            this.chromiumWebBrowser1.TabIndex = 3;
+            // 
+            // keywords
+            // 
+            this.keywords.Location = new System.Drawing.Point(10, 29);
+            this.keywords.MaxLength = 327670;
+            this.keywords.Multiline = true;
+            this.keywords.Name = "keywords";
+            this.keywords.Size = new System.Drawing.Size(407, 83);
+            this.keywords.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Keywords (Separated by comma)";
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip.Location = new System.Drawing.Point(0, 567);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(882, 22);
+            this.statusStrip.TabIndex = 6;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(10, 17);
+            this.toolStripStatusLabel1.Text = ".";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Preview";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.chromiumWebBrowser1);
+            this.panel1.Location = new System.Drawing.Point(10, 167);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(865, 358);
+            this.panel1.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "No of Posts to import";
+            // 
+            // txtPostCount
+            // 
+            this.txtPostCount.Location = new System.Drawing.Point(123, 122);
+            this.txtPostCount.Name = "txtPostCount";
+            this.txtPostCount.Size = new System.Drawing.Size(108, 20);
+            this.txtPostCount.TabIndex = 10;
+            this.txtPostCount.Text = "20";
+            // 
+            // txtHeader
+            // 
+            this.txtHeader.Location = new System.Drawing.Point(423, 29);
+            this.txtHeader.Multiline = true;
+            this.txtHeader.Name = "txtHeader";
+            this.txtHeader.Size = new System.Drawing.Size(452, 83);
+            this.txtHeader.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(420, 8);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(131, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Post header content (Use ";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // keyword
+            // 
+            this.keyword.AutoSize = true;
+            this.keyword.Location = new System.Drawing.Point(543, 8);
+            this.keyword.Name = "keyword";
+            this.keyword.Size = new System.Drawing.Size(75, 13);
+            this.keyword.TabIndex = 13;
+            this.keyword.TabStop = true;
+            this.keyword.Text = "<KEYWORD>";
+            this.keyword.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.keyword_LinkClicked);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(614, 8);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = " for placeholder)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 589);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.keyword);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtHeader);
+            this.Controls.Add(this.txtPostCount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.keywords);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Result2Post v1.4";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnClose;
+        private CefSharp.WinForms.ChromiumWebBrowser chromiumWebBrowser1;
+        private System.Windows.Forms.TextBox keywords;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPostCount;
+        private System.Windows.Forms.TextBox txtHeader;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.LinkLabel keyword;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Result2Post/Form1.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Web;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;    
+using CefSharp;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Text.RegularExpressions;
+using System.Configuration;
+using System.Management;
+using HAP = HtmlAgilityPack ;
+
+namespace WindowsFormsApp1
+{
+    // <div class="nrn-react-div" style="background-color:#fff;border:1px solid #292c2f47;box-shadow: 2px 2px 12px #aaaaaa69;border-right-color: #1da1f2;border-right-width: medium;margin-bottom: 13px;padding-left: 10px;padding-bottom: 10px;">
+    public partial class Form1 : Form
+    {
+        MySqlCommand command; MySqlConnection sqlConn;
+        string ConnectionString;
+        string wp_site,article_url,article_title;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool pageLoaded = false, resultFetched = false;
+       
+        public string GetSystemMACID()
+        {
+            string systemName = System.Windows.Forms.SystemInformation.ComputerName;
+            try
+            {
+                ManagementScope theScope = new ManagementScope("\\\\" + Environment.MachineName + "\\root\\cimv2");
+                ObjectQuery theQuery = new ObjectQuery("SELECT * FROM Win32_NetworkAdapter");
+                ManagementObjectSearcher theSearcher = new ManagementObjectSearcher(theScope, theQuery);
+                ManagementObjectCollection theCollectionOfResults = theSearcher.Get();
+
+                foreach (ManagementObject theCurrentObject in theCollectionOfResults)
+                {
+                    if (theCurrentObject["MACAddress"] != null)
+                    {
+                        string macAdd = theCurrentObject["MACAddress"].ToString();
+                        return macAdd.Replace(':', '-');
+                    }
+                }
+            }
+            catch (ManagementException e)
+            {
+            }
+            catch (System.UnauthorizedAccessException e)
+            {
+
+            }
+            return string.Empty;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string postHeader;
+
+            int maxPosts = int.Parse(txtPostCount.Text);
+            int postCategory = getDefaultCategory();
+
+            int postCount=1;
+            File.WriteAllText("output.html", "");
+            if (keywords.Text.Trim() == "")
+            {
+                MessageBox.Show("Please enter the keyword to search", "Import", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            
+            
+            
+
+            /*
+            var script = @"
+            document.getElementsByName('q')[0].value = 'CefSharp C# Example';
+            document.getElementById('search_button').click();
+               ";
+            script = @"
+            document.getElementById('rld-1').children[0].click();
+               ";
+            chromiumWebBrowser1.ExecuteScriptAsync(script);
+            */
+            string[] phrases = keywords.Text.Split(new string[] { "," }, StringSplitOptions.None);
+            string article="";
+            StringBuilder postContent=new StringBuilder("");
+            StringBuilder postFooter = new StringBuilder();
+            foreach (string phrase in phrases)
+            {
+                
+                postHeader = txtHeader.Text.Replace("<KEYWORD>", phrase);
+                postFooter = new StringBuilder("<h2 id=\"excerpts\">Excerpt Links</h2><p>");
+
+                toolStripStatusLabel1.Text = "Searhing for " + phrase + "...";
+                statusStrip.Refresh();
+                postContent = new StringBuilder(postHeader + "</br>");
+                string search_phrase = HttpUtility.UrlEncode(phrase); 
+                search_phrase = search_phrase.Replace(" ", "+");
+                postCount = 1;
+                pageLoaded = false;
+                chromiumWebBrowser1.LoadUrlAsync("https://duckduckgo.com/?q=" + search_phrase + "&ia=web").ContinueWith(x =>
+                {
+                    if (x.IsCompleted)
+                    {
+                        pageLoaded = true;
+                    }
+                });
+                while (!pageLoaded)
+                {
+                    Application.DoEvents();
+                }
+                
+                for (int i = 0; i <= 9; i++)
+                {
+                    // string script = @"document.getElementById('r1-0').innerHTML;";
+                    //chromiumWebBrowser1.ExecuteScriptAsync(script);
+                    resultFetched = false;
+                    article = "";
+                    chromiumWebBrowser1.EvaluateScriptAsync("document.getElementById('r1-" + i.ToString().Trim() + "').parentElement.outerHTML").ContinueWith(x =>
+                    {
+                        var response = x.Result;
+                        if (response.Success && response.Result != null)
+                        {
+                            //Saving the data to a database.
+                            resultFetched = true;
+                            article = response.Result.ToString();
+                        }
+                    },TaskScheduler.FromCurrentSynchronizationContext());
+                    Stopwatch stopwatch = new Stopwatch();
+                    stopwatch.Start();
+                    while (!resultFetched)
+                    {
+                        if (stopwatch.Elapsed.Seconds > 3 && !resultFetched)
+                            resultFetched = true;
+                        Application.DoEvents();
+                    }
+                    stopwatch.Stop();
+                    resultFetched = false;
+                    article_url    = getArticleLink(article);
+                    if (article_url == null) break;
+                    article_title = getArticleTitle(article);
+
+                    article = FormatArticle(article, postCount);
+                    postFooter.Append("("+ postCount.ToString() + "). <a id=\"excerpt" + postCount.ToString() + "\" href=\"" + article_url + "\" target=\"_blank\" rel=\"nofollow noopener\">"+ article_title + "</a><br>");
+
+                    File.AppendAllText("output.html", postCount.ToString() + ")   " + article + "\n\n\n\n\n\n");
+                    postContent.Append(article);
+                    postCount++;
+                    if (postCount > maxPosts)
+                        break;
+                } //Loop for first page result
+
+                var script = @"document.getElementById('rld-1').children[0].click();";
+                chromiumWebBrowser1.ExecuteScriptAsync(script);
+                if (postCount < maxPosts)
+                { 
+                    for (int i = 10; i <= 26; i++)
+                    {
+                        // string script = @"document.getElementById('r1-0').innerHTML;";
+                        //chromiumWebBrowser1.ExecuteScriptAsync(script);
+                        resultFetched = false;
+                        article = "";
+                        chromiumWebBrowser1.EvaluateScriptAsync("document.getElementById('r1-" + i.ToString().Trim() + "').parentElement.outerHTML").ContinueWith(x =>
+                        {
+                            var response = x.Result;
+                            if (response.Success && response.Result != null)
+                            {
+                                //Saving the data to a database.
+                                resultFetched = true;
+                                article = response.Result.ToString();
+                            }
+                        }, TaskScheduler.FromCurrentSynchronizationContext());
+                        Stopwatch stopwatch = new Stopwatch();
+                        stopwatch.Start();
+                        while (!resultFetched)
+                        {
+                            if (stopwatch.Elapsed.Seconds > 3 && !resultFetched)
+                                resultFetched = true;
+                            Application.DoEvents();
+                        }
+                        stopwatch.Stop();
+                        article_url = getArticleLink(article);
+                        if (article_url == null) break;
+                        article_title = getArticleTitle(article);
+
+                        article = FormatArticle(article, postCount);
+                        postFooter.Append("(" + postCount.ToString() + "). <a id=\"excerpt" + postCount.ToString() + "\" href=\"" + article_url + "\" target=\"_blank\" rel=\"nofollow noopener\">" + article_title + "</a><br>");
+
+                        File.AppendAllText("output.html", postCount.ToString() + ")   " + article + "\n\n\n\n\n\n");
+                        postContent.Append(article);
+                        postCount++;
+                        if (postCount > maxPosts)
+                            break;
+                    } //Loop for second page result
+                }
+
+                postFooter.Append("</p>"); 
+                postContent.Append(postFooter.ToString());
+
+                
+                if (true)
+                {
+                    string post_name = GenerateSlug(phrase);
+                    string query = "INSERT INTO wpuf_posts SET post_author=1,post_date=NOW(),post_date_gmt=UTC_TIMESTAMP(),post_content='" + postContent.ToString().Replace("'", "''").Replace(@"\'",@"\''") +
+                            "',post_excerpt ='',to_ping='',pinged ='',post_content_filtered ='',post_title='" + phrase.Replace("'","''").Replace(@"\'", @"\''") + "', " +
+                            "post_status='publish',comment_status='open',ping_status='open',post_name='" + post_name.Replace("'", "''").Replace(@"\'", @"\''") + "',post_modified=NOW(),post_modified_gmt=UTC_TIMESTAMP(), " +
+                            "post_parent=0,guid='',menu_order=0,post_type='post',comment_count=0";
+                    command = new MySqlCommand(query, sqlConn);
+                    command.ExecuteNonQuery();
+
+                    
+
+                    command = new MySqlCommand(query, sqlConn);
+                    command.CommandText = "SELECT last_insert_id()";
+                    object oResult = command.ExecuteScalar();
+                    int lastID = Convert.ToInt32(oResult);
+
+                    query = "INSERT INTO wpuf_term_relationships SET object_id="+ lastID.ToString()+ ",term_taxonomy_id="+ postCategory.ToString() + ",term_order=0";
+                    command = new MySqlCommand(query, sqlConn);
+                    command.ExecuteNonQuery();
+
+                    query = "UPDATE wpuf_posts SET guid='"+ wp_site + "?p=" + lastID.ToString() + "' WHERE ID=" + lastID.ToString();
+                    command = new MySqlCommand(query, sqlConn);
+                    command.ExecuteNonQuery();
+                }
+            } //Loop for each keyword
+            MessageBox.Show("Import has been finished","Import",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+
+        public int getDefaultCategory()
+        {
+            string query = "SELECT option_value FROM wpuf_options WHERE option_name = 'default_category'";
+            command = new MySqlCommand(query, sqlConn);
+            object oResult = command.ExecuteScalar();
+            int lastID = Convert.ToInt32(oResult);
+            return lastID;
+        }
+        public string getArticleLink(string article)
+        {
+            HAP.HtmlDocument html = new HAP.HtmlDocument();
+            html.LoadHtml(article);
+            HAP.HtmlNodeCollection nodes = html.DocumentNode.SelectNodes("//a[@data-testid='result-extras-url-link']");
+            if (nodes == null) return null;
+            return nodes[0].GetAttributeValue("href", "");
+        }
+
+        public string getArticleTitle(string article)
+        {
+            HAP.HtmlDocument html = new HAP.HtmlDocument();
+            html.LoadHtml(article);
+            return html.DocumentNode.SelectNodes("//h2")[0].InnerText;
+        }
+
+        public string FormatArticle(string article, int count)
+        {
+            HAP.HtmlDocument html = new HAP.HtmlDocument();
+            html.LoadHtml(article);
+            HAP.HtmlNodeCollection nodes = html.DocumentNode.SelectNodes ("//div/article/div");
+            nodes[0].Remove();
+            string title = html.DocumentNode.SelectNodes("//h2")[0].InnerText;
+            html.DocumentNode.SelectNodes("//h2")[0].InnerHtml = title;
+            html.DocumentNode.SelectNodes("//article")[0].SetAttributeValue("class", "ddg_article");
+            HAP.HtmlNode aNode = HAP.HtmlNode.CreateNode("<a href=\"#excerpt"+count.ToString() + "\" rel=\"noopener\"><sup>("+count.ToString()+")</sup></a>");
+            int spans = html.DocumentNode.SelectNodes("//span").Count;
+            html.DocumentNode.SelectNodes("//span")[spans-1].AppendChild(aNode);
+            return html.DocumentNode.InnerHtml;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void chromiumWebBrowser1_LoadingStateChanged(object sender, LoadingStateChangedEventArgs e)
+        {
+            if (!e.IsLoading)
+            {
+                pageLoaded = true;
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void keyword_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            txtHeader.SelectedText = "<KEYWORD>";
+        }
+
+        public string RemoveAccent( string txt)
+        {
+            byte[] bytes = System.Text.Encoding.GetEncoding("Cyrillic").GetBytes(txt);
+            return System.Text.Encoding.ASCII.GetString(bytes);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ConnectionString = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
+            sqlConn = new MySqlConnection(ConnectionString);
+            sqlConn.Open();
+            wp_site = ConfigurationManager.AppSettings["site"];
+            toolStripStatusLabel1.Text = GetSystemMACID();
+            statusStrip.Refresh();
+        }
+
+
+        public string GenerateSlug(string value)
+        {
+
+            //First to lower case
+            value = value.ToLowerInvariant();
+
+            //Remove all accents
+            var bytes = Encoding.GetEncoding("Cyrillic").GetBytes(value);
+            value = Encoding.ASCII.GetString(bytes);
+
+            //Replace spaces
+            value = Regex.Replace(value, @"\s", "-", RegexOptions.Compiled);
+
+            //Remove invalid chars
+            value = Regex.Replace(value, @"[^a-z0-9\s-_]", "", RegexOptions.Compiled);
+
+            //Trim dashes from end
+            value = value.Trim('-', '_');
+
+            //Replace double occurences of - or _
+            value = Regex.Replace(value, @"([-_]){2,}", "$1", RegexOptions.Compiled);
+
+            return value;
+        }
+
+        public string GenerateSlug_old( string phrase)
+        {
+            string str = RemoveAccent(phrase).ToLower();
+            // invalid chars           
+            str = Regex.Replace(str, @"[^a-z0-9\s-]", "");
+            // convert multiple spaces into one space   
+            str = Regex.Replace(str, @"\s+", " ").Trim();
+            // cut and trim 
+            str = str.Substring(0, str.Length <= 45 ? str.Length : 45).Trim();
+            str = Regex.Replace(str, @"\s", "-"); // hyphens   
+            return str;
+        }
+
+       
+    }
+
+
+}
